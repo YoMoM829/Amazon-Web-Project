01a8e53a17e3095b14f050be7e064baaca31c073
YoMoM829 <AnandVannalath@gmail.com>
2024-02-07 16:06:00 +1100

    Added all products to the homepage
    Added all the products to the homepage.

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,29 +1,3 @@
-const products = [{
-    image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    rating: {
-        stars: 45,
-        count: 87
-    },
-    price: 1090
-}, {
-    image: 'images/products/intermediate-composite-basketball.jpg',
-    name: 'Intermediate Size Basketball',
-    rating: {
-        stars: 40,
-        count: 127
-    },
-    price: 2095
-}, {
-    image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating: {
-        stars: 45,
-        count: 56
-    },
-    price: 799
-}];
-
 let productsHTML = '';
 
 products.forEach((product) => {
@@ -40,14 +14,14 @@ products.forEach((product) => {
 
             <div class="product-rating-container">
                 <img class="product-rating-stars"
-                    src="images/ratings/rating-${product.rating.stars}.png">
+                    src="images/ratings/rating-${product.rating.stars * 10}.png">
                 <div class="product-rating-count link-primary">
                     ${product.rating.count}
                 </div>
             </div>
 
             <div class="product-price">
-                $${(product.price / 100).toFixed(2)}
+                $${(product.priceCents / 100).toFixed(2)}
             </div>
 
             <div class="product-quantity-container">
