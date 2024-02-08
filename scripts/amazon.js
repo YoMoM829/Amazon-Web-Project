d18c5b5a1b29cdfc55bfdfcf2ed86a779774fe57
YoMoM829 <AnandVannalath@gmail.com>
2024-02-08 11:38:49 +1100

    Fixed the cart quantity
    The cart quantity now represents the number of products in the cart.

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -78,7 +78,14 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) =>
                 })
             }
 
-            console.log(cart);
+            let cartQuantity = 0;
+
+            cart.forEach((item) => {
+                cartQuantity += item.quantity;
+            })
+
+            document.querySelector('.js-cart-quantity').innerHTML =
+                cartQuantity;
         })
     }
 )
