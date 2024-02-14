5cac9bd2813a32ba7eade2924e13d8b593bdeaed
YoMoM829 <AnandVannalath@gmail.com>
2024-02-14 15:48:54 +1100

    Created automated tests for addToCart

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,22 @@
-export let cart = JSON.parse(localStorage.getItem('cart'));
+export let cart;
+
+loadFromStorage();
+
+export function loadFromStorage() {
+    cart = JSON.parse(localStorage.getItem('cart'));
+
+    if (!cart) {
+        cart = 
+            [{
+                productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+                quantity: 2,
+                deliveryOptionId: '1'
+            }, {
+                productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+                quantity: 1,
+                deliveryOptionId: '2'
+            }];
+    }}
 
 if (!cart) {
     cart = 
@@ -36,6 +54,8 @@ export function addToCart(productId, productQuantity) {
 
     saveToStorage();
 }
+
+
 
 export function removeFromCart(productId) {
     const newCart = [];
